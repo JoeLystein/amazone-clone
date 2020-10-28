6734d883a7fa4a6b255784a97ea1d5e90f931942
JoeLystein <70919712+JoeLystein@users.noreply.github.com>
2020-10-27 23:40:48 -0500

    product

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,30 @@
-import React from 'react';
-// import Ban3 from "./Ban3.jpg";
+import React from "react";
 import "./Home.css"
 import Product from "./Product";
 import Prod1 from "./prod1.jpg";
 
-function Home() {
-    return (
+function Home(){
+    return(
         <div className="home">
-            {/* <img className="home__image"   src={Ban3} alt=""/> */}
-            <img 
-                className="home__image"
-                src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
-                alt=""/>
+            {/* <div className="home__container"> */}
+                <img 
+                    className="home__image"
+                    src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+                    alt=""
+                /> 
+            {/* </div> */}
             <Product 
             id="12321341"
             title="The Learn Startup: How Constant Innovation Creates Radically Succesful Businesses Paperback"
             price={11.96}
             rating={5}
-            // image="https:://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
             image={Prod1}
             />
-
-            </div>
-        )
+            
+            
+            
+        </div>
+    ); 
 }
 
-export default Home
+export default Home;
