132e169c8649c330874e3b2ff62c7b6f80820588
JoeLystein <70919712+JoeLystein@users.noreply.github.com>
2020-11-05 13:13:37 -0600

    Stateprovider

--- src/Header.js
+++ src/Header.js
@@ -3,9 +3,12 @@ import "./Header.css"
 import { Link } from "react-router-dom"; 
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { useStateValue } from "./StateProvider";
 // import logo from './logoJ.png'
 
 function Header() {
+
+    const [{basket},dispatch]=useStateValue(); 
     return ( <nav className="header">
         <Link to="/">
             <img className="header_logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt=""/>
@@ -48,7 +51,7 @@ function Header() {
         <Link to="/checkout" className="header__link">
             <div className="header_optionBasket">
                 <ShoppingBasketIcon/>
-                <span className="header__optionLineTwo header__basketCount">0</span>
+                <span className="header__optionLineTwo header__basketCount">{basket.length}</span>
             </div>
         </Link>
 
