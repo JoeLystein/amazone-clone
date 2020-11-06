080486ac2ce6481f8fa63519364fe820ebe4842f
JoeLystein <70919712+JoeLystein@users.noreply.github.com>
2020-11-05 18:28:57 -0600

    authentication

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './App.css';
 import { BrowserRouter as Router, Switch,Route} from "react-router-dom";
 import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
-import Login from "./Login"
+import Login from "./Login";
+import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
+import {loadStripe} from "@stripe/stripe-js";
+
 
 function App() {
+
+  const[{user},dispatch] = useStateValue(); 
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>>", authUser);
+      
+      if(authUser) {
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        }); 
+      } 
+      else{
+          dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+
+    return () => {
+        unsubscribe();
+    }
+  },[]);
+
+
   return (
     <Router>
       <div className="app">
         <Switch>
           <Route path="/checkout">
             <Header/> 
-            {/* <h1>Checkout</h1> */}
             <Checkout/>
           </Route>
 
--- src/Header.js
+++ src/Header.js
@@ -4,12 +4,19 @@ import { Link } from "react-router-dom";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
 // import logo from './logoJ.png'
 
 function Header() {
-
-    const [{basket},dispatch]=useStateValue(); 
+    const [{basket,user}]=useStateValue(); 
     console.log(basket);
+
+    const handleAuthentication = () =>{
+        if(user){
+            auth.signOut();
+        }
+    }
+
     return ( <nav className="header">
         <Link to="/">
             <img className="header_logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt=""/>
@@ -22,10 +29,10 @@ function Header() {
         </div>
         
         <div className="header__nav">
-            <Link to="/login" className="header__link">
-                <div className="header__option">
-                    <span className="header__optionLineOne">Hello Joel</span>
-                    <span className="header__optionLineTwo">Sign In</span>
+            <Link to={!user && "/login"} className="header__link">
+                <div onClick={ handleAuthentication  } className="header__option">
+                    <span className="header__optionLineOne">Hello {!user ? 'Guest' : user.email}</span>
+                    <span className="header__optionLineTwo">{user ? 'Sign Out' : 'Sign In'}</span>
                 </div>
             </Link>
         </div>
